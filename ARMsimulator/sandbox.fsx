681b6cf7c57060a6bb794735ce4c812f3c87c6a2
Sze Tyng Lee <lee.szetyng@gmail.com>
2018-02-08 23:03:12 +0000

    fixed noob github errors

--- ARMsimulator/sandbox.fsx
+++ ARMsimulator/sandbox.fsx
@@ -13,5 +13,19 @@ open Memory
 open CommonTop
 // open Test
 
+let asmLine = "LDRB R10, [R15, #5]!"
 
+let splitIntoWords ( line:string ) =
+    line.Split( ([||] : char array), 
+        System.StringSplitOptions.RemoveEmptyEntries)
+
+let makeOperands words =
+    match words with
+    | opc :: operands -> String.concat "" operands
+    | _ -> failwithf "NO"
+
+let operands = splitIntoWords asmLine |> Array.toList |> makeOperands
+let opList = operands.Split(',') |> Array.toList
+let op2 = opList.[2]
+op2.Contains("!")
 
