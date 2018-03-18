24965beb040b0a1102716b7241985a7974ebed5b
Sze Tyng Lee <lee.szetyng@gmail.com>
2018-03-18 18:11:02 +0000

    multipass working, example in multipassexec.fsx

--- VisualTest/CommonTop.fs
+++ VisualTest/CommonTop.fs
@@ -80,4 +80,72 @@ let parseLine (symtab: SymbolTable option) (loadAddr: WAddr) (asmLine:string) =
     |> Array.toList
     |> matchLine
 
+let multiParseLine (symtab: SymbolTable option) (loadAddr: WAddr) (asmMultiLine:string []): Result<Parse<Instr>,string>list =
+    /// put parameters into a LineData record
+    let makeLineData opcode operands = {
+        OpCode=opcode
+        Operands=String.concat "" operands
+        Label=None
+        LoadAddr = loadAddr
+        SymTab = symtab
+    }
+    /// split multiline separated by \n into an array
+    /// each element of the array is a newline
+    let splitIntoLines ( line:string ) =
+                line.Split( ([|'\n'|] : char array), 
+                    System.StringSplitOptions.RemoveEmptyEntries)
+    /// remove comments from string
+    let removeComment (txt:string) =
+        txt.Split(';')
+        |> function 
+            | [|x|] -> x 
+            | [||] -> "" 
+            | lineWithComment -> lineWithComment.[0]
+    /// split line on whitespace into an array
+    let splitIntoWords ( line:string ) =
+        line.Split( ([||] : char array), 
+            System.StringSplitOptions.RemoveEmptyEntries)
+    /// try to parse 1st word, or 2nd word, as opcode
+    /// If 2nd word is opcode 1st word must be label
+    let secondPass data =
+        match data |> IMatch with
+        | Some pa -> pa
+        | None -> failwithf "idk"
 
+    let firstPass prevLD src = 
+        let currAddr = 
+            match prevLD.LoadAddr with
+            | WA a -> a
+        let currSymTab = prevLD.SymTab        
+        match src with
+        | label :: opc :: operands ->
+            match Map.tryFind label SingleR.opCodes with
+            | Some _ -> {makeLineData opc operands 
+                            with LoadAddr=WA currAddr}, {prevLD with LoadAddr=WA(currAddr+4u)}
+            | None -> 
+                let newSymTab = 
+                    match currSymTab,currAddr with
+                    | None, a -> Some (Map.ofList [label,a])
+                    | Some s, a -> Some (Map.add label a s)
+                {makeLineData opc operands
+                    with LoadAddr=WA currAddr;Label=Some label;SymTab=newSymTab}, {prevLD with LoadAddr=WA(currAddr+4u);SymTab=newSymTab}                        
+
+    let dummyLD = {LoadAddr=loadAddr ; Label=None ; SymTab=None ; OpCode="" ; Operands=""}                 
+    let asmSplitLineSplitWords = 
+        asmMultiLine
+        //|> splitIntoLines  
+        |> Array.toList 
+        |> List.map (removeComment >> splitIntoWords >> Array.toList)
+    let listLineData, finalLineData = List.mapFold firstPass dummyLD asmSplitLineSplitWords 
+    let listLineDataWSymTab = List.map (fun d -> {d with SymTab=finalLineData.SymTab}) listLineData
+    List.map secondPass listLineDataWSymTab
+
+/// Assume that program has been parsed and is valid.
+let myParseAndExecute tD asm= 
+    let parsedResList = multiParseLine None (WA 0ul) asm
+    let exec tD parsedRes = 
+        match parsedRes, tD with
+        | Ok x, Ok d -> IExecute x.PInstr d
+        | Error e, _ -> Error e
+        | _, Error e -> Error e
+    List.fold exec tD parsedResList
